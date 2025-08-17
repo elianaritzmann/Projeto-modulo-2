b3954d450c518b8c4e8c721ab2f8c40df1e8cc22
Eliana Ritzmann <eliana.cristinas2@hotmail.com>
2025-08-17 18:28:11 -0300

    Tela de pontos de coleta feita (aparencia)
    Tela pronta, falta adicionar as funcionalidades

--- src/pages/Places/Places.jsx
+++ src/pages/Places/Places.jsx
@@ -1,5 +1,34 @@
+import CardPontoColeta from '../../components/CardPontoColeta';
+import './Places.css'
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 function Places() {
-  return <>eu sou tela de locais</>;
+  return (
+    <div className='containerPlaces'>
+      <h1 className='seusPontos'>Seus pontos de Coleta</h1>
+      <div className='alinharBotao'>
+          <button className='novoPonto'> Novo
+         <AddOutlinedIcon></AddOutlinedIcon>
+        
+      </button>
+      </div>
+      
+     
+      <div className='cardsPontos'>
+          <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+           <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+           <CardPontoColeta></CardPontoColeta>
+          <CardPontoColeta></CardPontoColeta>
+        
+        
+      </div>
+    </div>
+  )
 }
 
 export default Places;
