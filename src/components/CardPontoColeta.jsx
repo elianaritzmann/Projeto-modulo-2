7749b74be2585d44284061bd6a497b4dae9218b6
Eliana Ritzmann <eliana.cristinas2@hotmail.com>
2025-08-21 19:41:40 -0300

    Places agora com funcionalidades
    listagem de pontos de coleta do usuário agora funcionando

--- src/components/CardPontoColeta.jsx
+++ src/components/CardPontoColeta.jsx
@@ -2,17 +2,21 @@ import './CardPontoColeta.css'
 import DeleteIcon from '@mui/icons-material/Delete';
 import PlaceIcon from '@mui/icons-material/Place';
 import RecyclingIcon from '@mui/icons-material/Recycling';
-function CardPontoColeta(){
+function CardPontoColeta(props){
         return(
             <div className='containerPonto'>
               <div>
-                <h3>Nome do ponto</h3>
-                <div className='iconeTexto'> <PlaceIcon></PlaceIcon> <p>rua tal tal tal</p></div>
-                <div className='iconeTexto'><RecyclingIcon></RecyclingIcon> <p>Materiais</p></div>
-              </div>  
+                <h3>{props.nome}</h3>
+                <div className='iconeTexto'> 
+                  <PlaceIcon></PlaceIcon>
+                   <p>  {props.endereco.logradouro}, {props.endereco.bairro} - {props.endereco.localidade}/{props.endereco.uf}, CEP: {props.endereco.cep}</p></div>
+                <div className='iconeTexto'>
+                  <RecyclingIcon></RecyclingIcon> 
+                  <p>{props.materiais}</p></div>
+              </div> 
                 <div className='botaoExcluir'>
                    <DeleteIcon></DeleteIcon>
-                </div>
+              </div>
                 
             </div>
         )
--- src/pages/Places/Places.jsx
+++ src/pages/Places/Places.jsx
@@ -1,32 +1,55 @@
+import { useEffect, useState } from 'react';
 import CardPontoColeta from '../../components/CardPontoColeta';
+import RegisterPlace from '../RegisterPlace/RegisterPlace';
 import './Places.css'
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Places() {
+  
+  const [locais, setLocais] = useState([])
+
+  useEffect(() => {
+    const usuario = (JSON.parse(localStorage.getItem('usuario'))) || []
+     axios.get('http://localhost:3000/coletas', { 
+    headers: {
+        usuarioid: usuario.usuario.id,
+        "Content-Type": "application/json"
+      }}
+  ).then((response)=>{
+      setLocais(response.data)
+      console.log(response.data)
+  })
+  }, []);         
   return (
     <div className='containerPlaces'>
       <h1 className='seusPontos'>Seus pontos de Coleta</h1>
       <div className='alinharBotao'>
-          <button className='novoPonto'> Novo
-         <AddOutlinedIcon></AddOutlinedIcon>
-        
-      </button>
+          <Link to= "/locais/novo" Component={RegisterPlace} className='novoPonto'>
+          <h3>Novo</h3>
+            <AddOutlinedIcon></AddOutlinedIcon>
+         </Link>
       </div>
-      
-     
+
       <div className='cardsPontos'>
-          <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-           <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-           <CardPontoColeta></CardPontoColeta>
-          <CardPontoColeta></CardPontoColeta>
-        
-        
-      </div>
+  {locais.length > 0 ? (
+    locais.map((local) => {
+      return (
+        <CardPontoColeta
+          key={local.id} // melhor que usar o índice
+          nome={local.nome}
+          endereco={local.endereco}
+          materiais={local.residuos}
+        />
+      )
+    })
+  ) : (
+    <p>Você ainda não tem pontos de coleta cadastrados.</p>
+  )}
+</div>
     </div>
   )
 }
